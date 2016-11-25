c5535030fcaec1e74be9911281ca7bef23e8c65e
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-25 03:05:45 -0600

    Add mongodb connection for schools data.

--- server/server.js
+++ server/server.js
@@ -1,34 +1,30 @@
 import express from 'express';
 import path from 'path';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 
 app.use(express.static('dist'));
+
+const url4650 = `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASS}@${process.env.MLAB_URL}/4650`;
+let db4650;
+
+MongoClient.connect(url4650, (err, database) => {
+    db4650 = database;
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Running on port ${port}`);
+    });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 app.get('/data/schools', (req, res) => {
-    const tempdata = [
-        {
-            id: 'MUSIC',
-            name: 'Bienen'
-        },
-        {
-            id: 'SoC',
-            name: 'Comm'
-        },
-        {
-            id: 'MEAS',
-            name: 'McCormick'
-        }
-    ];
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(tempdata));
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
+    db4650.collection('schools').find().toArray((err, result) => {
+        if (err) console.log(err);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));
+    });
 });
