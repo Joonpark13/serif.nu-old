086aeccc3d7a99e4c1e3127bee3430dfc5f25051
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-25 12:48:29 -0600

    Implement sections and details data urls.

--- server/server.js
+++ server/server.js
@@ -48,3 +48,30 @@ app.get('/data/courses/:school/:subject', (req, res) => {
         res.send(JSON.stringify(result));
     });
 });
+
+app.get('/data/sections/:school/:subject/:course', (req, res) => {
+    db4650.collection('sections').find(
+        {
+            school: req.params.school,
+            subject: req.params.subject,
+            course: req.params.course
+        }
+    ).toArray((err, result) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));
+    });
+});
+
+app.get('/data/details/:school/:subject/:course/:section', (req, res) => {
+    db4650.collection('details').find(
+        {
+            school: req.params.school,
+            subject: req.params.subject,
+            course: req.params.course,
+            section: req.params.section
+        }
+    ).toArray((err, result) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));
+    });
+});
