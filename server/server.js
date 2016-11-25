5785fcbeab227c2f6b6b1681e5cd83a460a8fd34
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-25 02:34:00 -0600

    Refactor using express get rerquest instead of static file serve.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
