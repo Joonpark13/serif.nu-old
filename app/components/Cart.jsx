556e0844fa3cb8528221622eaa41027af794b91e
Joon Park <JoonPark2017@u.northwestern.edu>
2016-12-20 01:55:37 -0500

    Implement swapping components.

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, CardHeader, CardText, CardActions } from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
+import Components from './Components.jsx';
+
 const style = {
   card: {
     marginBottom: '10px'
@@ -12,37 +14,64 @@ const matchId = (id) => (
   (component) => (id === component.id)
 );
 
-const Cart = ({ sections, components, remove }) => (
+const Cart = ({
+  isFetching,
+  selected,
+  sections,
+  components,
+  details,
+  swapping,
+  remove,
+  swapComponent,
+  addComponent
+}) => (
   <div>
     {sections.map((section) => {
       // There should at most be one match
       const component = components.filter(matchId(section.id))[0];
       return (
-        <Card key={section.id} style={style.card}>
-          <CardHeader
-            title={section.name}
-            subtitle={`${section.subject} ${section.course}`}
-            actAsExpander
-            showExpandableButton
-          />
-          <CardText expandable>
-            <p>{section.meeting_time}</p>
-            <p>{section.instructor.join(', ')}</p>
-            {section.topic && <p>{section.topic}</p>}
-            <p>{section.overview_of_class}</p>
-            <p>ID: {section.id}</p>
-            {component && (
-              <div>
-                <h4>{component.component}</h4>
-                <p>{component.meeting_time}</p>
-                <p>{component.room}</p>
-              </div>
-            )}
-          </CardText>
-          <CardActions>
-            <FlatButton label="Remove" primary onTouchTap={() => remove(section.id)} />
-          </CardActions>
-        </Card>
+        <div key={section.id}>
+          {!isFetching && swapping && <Components
+            sections={sections}
+            selected={selected}
+            details={details}
+            addComponent={addComponent}
+          />}
+          {!swapping &&
+            <Card style={style.card}>
+              <CardHeader
+                title={section.name}
+                subtitle={`${section.subject} ${section.course}`}
+                actAsExpander
+                showExpandableButton
+              />
+              <CardText expandable>
+                <p>{section.meeting_time}</p>
+                <p>{section.instructor.join(', ')}</p>
+                {section.topic && <p>{section.topic}</p>}
+                <p>{section.overview_of_class}</p>
+                <p>ID: {section.id}</p>
+                {component && (
+                  <div>
+                    <h4>{component.component}</h4>
+                    <p>{component.meeting_time}</p>
+                    <p>{component.room}</p>
+                  </div>
+                )}
+              </CardText>
+              <CardActions>
+                <FlatButton label="Remove" primary onTouchTap={() => remove(section.id)} />
+                {component && (
+                  <FlatButton
+                    label="Swap Component"
+                    primary
+                    onTouchTap={() => swapComponent(section.school, section.subject, section.course, section.id)}
+                  />
+                )}
+              </CardActions>
+            </Card>
+          }
+        </div>
       );
     })}
   </div>
@@ -53,5 +82,7 @@ export default Cart;
 Cart.propTypes = {
   sections: React.PropTypes.arrayOf(React.PropTypes.object),
   components: React.PropTypes.arrayOf(React.PropTypes.object),
-  remove: React.PropTypes.func
+  swapping: React.PropTypes.bool,
+  remove: React.PropTypes.func,
+  swapComponent: React.PropTypes.func
 };
--- app/reducers/reducers.js
+++ app/reducers/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import browse from './browse';
 import search from './search';
 import calendar from './calendar';
+import cart from './cart';
 
 const reducer = combineReducers({
   browse,
   search,
-  calendar
+  calendar,
+  cart
 });
 
 export default reducer;
