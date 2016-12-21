7367781bf5e53db33cb456cb94646a1a5200708f
Joon Park <JoonPark2017@u.northwestern.edu>
2016-12-21 15:34:15 -0500

    Implement localStorage.

--- app/store.js
+++ app/store.js
@@ -1,12 +1,14 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import reducer from './reducers/reducers';
+import persistState from 'redux-localstorage';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducer,
     composeEnhancers(
-        applyMiddleware(thunkMiddleware)
+        applyMiddleware(thunkMiddleware),
+        persistState('calendar')
     )
 );
 
