b9a855c2bfec2cf3e2078ad0a86b1f163a392357
Joon Park <JoonPark2017@u.northwestern.edu>
2016-12-21 20:07:06 -0500

    Fix multiple components appearing on reload.

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -29,53 +29,51 @@ const Cart = ({
 }) => (
   <div>
     {sections.length === 0 && <h3>Cart is empty</h3>}
-    {sections.map((section) => {
+    {!isFetching && swapping && <Components
+      sections={sections}
+      selected={selected}
+      details={details}
+      addComponent={addComponent}
+      addComponentHover={addComponentHover}
+      removeHover={removeHover}
+    />}
+    {!swapping && sections.map((section) => {
       // There should at most be one match
       const component = components.filter(matchId(section.id))[0];
       return (
         <div key={section.id}>
-          {!isFetching && swapping && <Components
-            sections={sections}
-            selected={selected}
-            details={details}
-            addComponent={addComponent}
-            addComponentHover={addComponentHover}
-            removeHover={removeHover}
-          />}
-          {!swapping &&
-            <Card style={style.card}>
-              <CardHeader
-                title={section.name}
-                subtitle={`${section.subject} ${section.course}`}
-                actAsExpander
-                showExpandableButton
-              />
-              <CardText expandable>
-                <p>{section.meeting_time}</p>
-                <p>{section.instructor.join(', ')}</p>
-                {section.topic && <p>{section.topic}</p>}
-                <p>{section.overview_of_class}</p>
-                <p>ID: {section.id}</p>
-                {component && (
-                  <div>
-                    <h4>{component.component}</h4>
-                    <p>{component.meeting_time}</p>
-                    <p>{component.room}</p>
-                  </div>
-                )}
-              </CardText>
-              <CardActions>
-                <FlatButton label="Remove" primary onTouchTap={() => remove(section.id)} />
-                {component && (
-                  <FlatButton
-                    label="Swap Component"
-                    primary
-                    onTouchTap={() => swapComponent(section.school, section.subject, section.course, section.id)}
-                  />
-                )}
-              </CardActions>
-            </Card>
-          }
+          <Card style={style.card}>
+            <CardHeader
+              title={section.name}
+              subtitle={`${section.subject} ${section.course}`}
+              actAsExpander
+              showExpandableButton
+            />
+            <CardText expandable>
+              <p>{section.meeting_time}</p>
+              <p>{section.instructor.join(', ')}</p>
+              {section.topic && <p>{section.topic}</p>}
+              <p>{section.overview_of_class}</p>
+              <p>ID: {section.id}</p>
+              {component && (
+                <div>
+                  <h4>{component.component}</h4>
+                  <p>{component.meeting_time}</p>
+                  <p>{component.room}</p>
+                </div>
+              )}
+            </CardText>
+            <CardActions>
+              <FlatButton label="Remove" primary onTouchTap={() => remove(section.id)} />
+              {component && (
+                <FlatButton
+                  label="Swap Component"
+                  primary
+                  onTouchTap={() => swapComponent(section.school, section.subject, section.course, section.id)}
+                />
+              )}
+            </CardActions>
+          </Card>
         </div>
       );
     })}
