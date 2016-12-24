768b9c7c348983c6000ca8eb9aacbd462bc575fc
Joon Park <JoonPark2017@u.northwestern.edu>
2016-12-23 22:58:21 -0500

    React production plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -27,5 +27,13 @@ module.exports = {
                 loader: 'url-loader?limit=100000'
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
 };
