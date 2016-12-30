ad8dd97d406846869dde5f6cb0fc745f05fb8c45
Joon Park <JoonPark2017@u.northwestern.edu>
2016-12-30 02:23:21 -0500

    Better search result formatting.

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import AutoComplete from 'material-ui/AutoComplete';
 
 const combineFilters = (searchText, key) => {
-  return AutoComplete.caseInsensitiveFilter(searchText, key) || AutoComplete.fuzzyFilter(searchText, key);
+  if (searchText.length < 3) return false; // Set min character number for search
+  return AutoComplete.caseInsensitiveFilter(searchText, key);
 };
 
 const Search = ({ currentTerm, searchData, onSelect }) => (
   <AutoComplete
     hintText="Search for classes"
     dataSource={searchData}
-    maxSearchResults={12}
+    // maxSearchResults={12}
     filter={combineFilters}
     fullWidth
     onNewRequest={(chosenRequest, index) => {
@@ -17,6 +18,10 @@ const Search = ({ currentTerm, searchData, onSelect }) => (
         onSelect(currentTerm, chosenRequest.school, chosenRequest.subject, chosenRequest.course);
       }
     }}
+    // textFieldStyle={{ fullWidth: true }}
+    menuStyle={{ height: '500px' }}
+    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+    targetOrigin={{ vertical: 'top', horizontal: 'right' }}
   />
 );
 
