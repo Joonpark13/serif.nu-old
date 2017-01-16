91fe476bd2ee086f5136b7a2f033157b07075b0b
Joon Park <JoonPark2017@u.northwestern.edu>
2017-01-16 13:03:36 -0600

    Update facebook icon button.
    Facebook icon button is now the official facebook color instead of the
    default icon button color. Disabled state still turns the icon gray.

--- app/components/CalendarHeader.jsx
+++ app/components/CalendarHeader.jsx
@@ -8,7 +8,6 @@ import $ from 'jquery';
 
 import html2canvas from '../js/html2canvas';
 import { northwesternPurple } from '../colors';
-import googleCalendar from '../images/logo_calendar_64px.png';
 
 const style = {
   header: {
@@ -38,8 +37,7 @@ const style = {
   },
   messageBox: {
     width: '450px'
-  },
-  googleCalendar: { width: '24px', height: '24px' }
+  }
 };
 
 // http://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
@@ -187,7 +185,7 @@ class CalendarHeader extends React.Component {
               onTouchTap={() => this.props.handleAuth()}
               disabled={!this.props.hasClasses}
             >
-              <img src={googleCalendar} style={style.googleCalendar} alt="Google Calendar" />
+              <FontIcon className="material-icons">event_note</FontIcon>
             </IconButton>
             <IconButton
               tooltip="Remove Calendar"
