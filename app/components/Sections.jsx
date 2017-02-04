c269ff49fca592a412c52ae9b730d9086b75e323
Joon Park <JoonPark2017@u.northwestern.edu>
2017-02-04 16:07:42 -0600

    Add checkbox to show class descriptions
    Users can now browse sections with the sections' class descriptions displayed by the use of a checkbox on top of the list of sections.

--- app/components/Sections.jsx
+++ app/components/Sections.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { List, ListItem } from 'material-ui/List';
+import Checkbox from 'material-ui/Checkbox';
 
 import { inCalendar } from '../helpers';
 
@@ -26,58 +27,86 @@ const style = {
   },
   bodyText: {
     fontSize: 'small'
+  },
+  checkbox: {
+    paddingLeft: '5px'
   }
 };
 
-const Sections = ({
-  currentTerm,
-  currentCalendar,
-  selected,
-  sections,
-  calendar,
-  checkComponents,
-  addCourse,
-  addCourseHover,
-  removeHover
-}) => (
-  <div>
-    <h3 style={style.headings}>Choose a section:</h3>
+export default class Sections extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkboxOpen: false
+    };
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+  }
+  handleCheckbox() {
+    this.setState({
+      checkboxOpen: !this.state.checkboxOpen
+    });
+  }
+  render() {
+    const {
+      currentTerm,
+      currentCalendar,
+      selected,
+      sections,
+      calendar,
+      checkComponents,
+      addCourse,
+      addCourseHover,
+      removeHover
+    } = this.props;
+    return (
+      <div>
+        <h3 style={style.headings}>Choose a section:</h3>
+        <Checkbox
+          checked={this.state.checkboxOpen}
+          label="Show descriptions"
+          onCheck={() => this.handleCheckbox()}
+          style={style.checkbox}
+        />
 
-    <List>
-      {sections.map(section => {
-        const inCal = inCalendar(calendar.get('sections'), section.id, currentTerm, currentCalendar);
-        return (
-          <ListItem
-            key={section.section}
-            // Make sure the section is not already in calendar
-            disabled={inCal}
-            onMouseEnter={() => {
-              if (!inCal) {
-                addCourseHover(section);
-              }
-            }}
-            onMouseLeave={() => {
-              if (!inCal) {
-                removeHover(section.id);
-              }
-            }}
-            onTouchTap={() => {
-              checkComponents(currentTerm, selected.school, selected.subject, selected.course, section.id);
-              addCourse(section);
-              removeHover(section.id);
-            }}
-          >
-            <h4 style={style.title}>{`Section ${section.section}`}</h4>
-            {section.topic && <p>{section.topic}</p>}
-            <p style={style.bodyText}>{section.meeting_time}</p>
-            <p style={style.bodyText}>{section.location}</p>
-            <p style={style.bodyText}>{section.instructor.join(', ')}</p>
-          </ListItem>
-         );
-      })}
-    </List>
-  </div>
-);
+        <List>
+          {sections.map(section => {
+            const inCal = inCalendar(calendar.get('sections'), section.id, currentTerm, currentCalendar);
+            const courseDesc = section.overview_of_class ? section.overview_of_class : 'No description available.';
+            return (
+              <ListItem
+                key={section.section}
+                // Make sure the section is not already in calendar
+                disabled={inCal}
+                onMouseEnter={() => {
+                  if (!inCal) {
+                    addCourseHover(section);
+                  }
+                }}
+                onMouseLeave={() => {
+                  if (!inCal) {
+                    removeHover(section.id);
+                  }
+                }}
+                onTouchTap={() => {
+                  checkComponents(currentTerm, selected.school, selected.subject, selected.course, section.id);
+                  addCourse(section);
+                  removeHover(section.id);
+                }}
+              >
+                <h4 style={style.title}>{`Section ${section.section}`}</h4>
+                {section.topic && <p>{section.topic}</p>}
+                <p style={style.bodyText}>{section.meeting_time}</p>
+                <p style={style.bodyText}>{section.location}</p>
+                <p style={style.bodyText}>{section.instructor.join(', ')}</p>
+                {this.state.checkboxOpen && <p style={style.bodyText}>{courseDesc}</p>}
+              </ListItem>
+            );
+          })}
+        </List>
+      </div>
+    );
+  }
+}
 
 Sections.propTypes = {
   currentTerm: React.PropTypes.string,
@@ -98,5 +127,3 @@ Sections.propTypes = {
   removeHover: React.PropTypes.func,
   currentCalendar: React.PropTypes.number
 };
-
-export default Sections;
